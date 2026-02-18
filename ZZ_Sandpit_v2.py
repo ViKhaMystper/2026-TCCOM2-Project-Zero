2b438e3fe4e409f62e5030618f46d188b4e86121
ViKhaMystper <tranb4547@masseyhigh.school.nz>
2026-02-19 10:17:44 +1300

    string checker

---
+++ ZZ_Sandpit_v2.py
@@ -0,0 +1,16 @@
+#list exploration/ experiment
+
+fruit_list = ["apple", "banana", "cherry, dragon fruit"]
+
+for item in fruit_list:
+
+    print()
+
+    # print the whole word
+    print("Fruit name: ", item)
+
+    # print the first letter
+    print("First letter: ", item[0])
+
+    # challenge - Print the first TWO letter
+    print(f"First two letters  {item[:3]}")
